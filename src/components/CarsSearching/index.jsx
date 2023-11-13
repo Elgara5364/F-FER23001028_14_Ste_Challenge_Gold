baa9896e7d2d93f795f8b4d615dd29189c9a5ef7
Elgara5364 <steveloardi5364@gmail.com>
2023-11-13 16:48:53 +0800

    fix input car name on html

--- src/components/CarsSearching/index.jsx
+++ src/components/CarsSearching/index.jsx
@@ -9,14 +9,15 @@ const CarsSearching = () => {
   const [title, setTitle] = useState("");
   const [category, setCategory] = useState("");
   const [price, setPrice] = useState("");
-  const [isRented, setIsRented] = useState();
+  const [isRented, setIsRented] = useState("");
+  const [isFilter, setisFilter] = useState(false);
 
   useEffect(() => {
-    handleGetList();
+    handleGetList(title, category, price, isRented);
   }, []);
 
   const handleChangeTitle = (e) => {
-    console.log(e);
+    // console.log(e);
     setTitle(e.target.value);
   };
 
@@ -32,10 +33,26 @@ const CarsSearching = () => {
     setIsRented(e.target.value);
   };
 
-  const handleGetList = () => {
+  const handleSubmit = (e) => {
+    // console.log(title);
+    e.preventDefault();
+    handleGetList(title, category, price, isRented);
+    setisFilter(true);
+  };
+
+  const handleReset = () => {
+    setTitle("");
+    setCategory("");
+    setPrice("");
+    setIsRented("");
+    handleGetList("", "", "", "");
+    setisFilter(false);
+  };
+
+  const handleGetList = (dataTitle, dataCategory, dataPrice, dataIsRented) => {
     axios
       .get(
-        `https://api-car-rental.binaracademy.org/customer/v2/car?name=${title}&category=${category}&price=${price}&isRented=${isRented}`
+        `https://api-car-rental.binaracademy.org/customer/v2/car?name=${dataTitle}&category=${dataCategory}&price=${dataPrice}&isRented=${dataIsRented}`
       )
       // .get(`https://api.mudoapi.tech/menus?name=${name}&type=${type}`)
       .then((res) => {
@@ -44,11 +61,6 @@ const CarsSearching = () => {
         // setCarName(res.data);
       })
       .catch((err) => console.log(err));
-  };
-
-  const handleSubmit = () => {
-    // console.log(title);
-    handleGetList();
   };
 
   return (
@@ -61,6 +73,7 @@ const CarsSearching = () => {
             <input
               onChange={handleChangeTitle}
               className="title"
+              value={title}
               placeholder="Ketik nama/ Tipe Mobil"
             />
           </div>
@@ -68,9 +81,7 @@ const CarsSearching = () => {
             <h4>Kategori</h4>
             <div className="category">
               <select value={category} onChange={searchCategory}>
-                <option className="title" value="">
-                  Masukan Kapasitas Mobil
-                </option>
+                <option value="">Masukan Kapasitas Mobil</option>
                 <option value="small">1-4 org</option>
                 <option value="medium">4-6 org</option>
                 <option value="large">6-8 org</option>
@@ -81,9 +92,7 @@ const CarsSearching = () => {
             <h4>Harga</h4>
             <div className="price">
               <select value={price} onChange={searchPrice}>
-                <option className="title" value="">
-                  Masukkan Harga Sewa per Hari
-                </option>
+                <option value="">Masukkan Harga Sewa per Hari</option>
                 <option value="minPrice=0&maxPrice=400000">
                   &lt; Rp. 400.000
                 </option>
@@ -100,40 +109,38 @@ const CarsSearching = () => {
             <h4>Status</h4>
             <div className="status">
               <select value={isRented} onChange={searchIsRented}>
-                <option value={true}>Disewa</option>
-                <option value={false}>Tidak Disewa</option>
+                <option value="true">Disewa</option>
+                <option value="false">Tidak Disewa</option>
               </select>
             </div>
           </div>
           <div className="btn">
-            <button onClick={handleSubmit}>Cari Mobil</button>
+            <button onClick={isFilter ? handleReset : handleSubmit}>
+              {isFilter ? "Reset" : "Cari Mobil"}
+            </button>
           </div>
         </div>
       </div>
       <div className="list-car">
-        {title.length ? (
-          <h1>Data Kosong</h1>
-        ) : (
-          listCar.map((data) => (
-            <div className="list">
-              <img src={data.image} alt="" />
-              <div className="desc">
-                <h5>{data.name}</h5>
-                <h1>{data.price} / hari</h1>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                  Possimus qui repellendus consequuntur id, suscipit provident
-                  neque dignissimos nostrum? Culpa, sunt!
-                </p>
-                <div className="list-btn">
-                  <Link to={`/cars-details/${data.id}`}>
-                    <button>pilih mobil</button>
-                  </Link>
-                </div>
+        {listCar.map((data) => (
+          <div className="list">
+            <img src={data.image} alt="" />
+            <div className="desc">
+              <h5>{data.name}</h5>
+              <h1>{data.price} / hari</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Possimus qui repellendus consequuntur id, suscipit provident
+                neque dignissimos nostrum? Culpa, sunt!
+              </p>
+              <div className="list-btn">
+                <Link to={`/cars-details/${data.id}`}>
+                  <button>Pilih Mobil</button>
+                </Link>
               </div>
             </div>
-          ))
-        )}
+          </div>
+        ))}
       </div>
     </div>
   );
