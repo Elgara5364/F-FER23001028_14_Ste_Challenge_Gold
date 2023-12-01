d6f117efb7b6ad46d3355c2ea906fda539226d13
Elgara5364 <steveloardi5364@gmail.com>
2023-12-01 21:09:16 +0900

    fix image in OurServices and fix sidebar on homepage

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -3,16 +3,8 @@ import { HashLink } from "react-router-hash-link";
 import "./style.css";
 import { Link, useNavigate } from "react-router-dom";
 import Sidebar from "../Sidebar";
-import Menu from "../../assets/icon/fi_menu.png";
-import Close from "../../assets/icon/fi_x.png";
 
 const Navbar = () => {
-  const [showSidebar, setshowSidebar] = useState(false);
-
-  const handleSidebar = () => {
-    setshowSidebar(!showSidebar);
-  };
-
   const access_Token = localStorage.getItem("Acces Token");
   const navigate = useNavigate();
 
@@ -26,42 +18,35 @@ const Navbar = () => {
       <Link to={"/"}>
         <div className="logo"></div>
       </Link>
-      <div className="ham-menu">
-        {showSidebar && <Sidebar />}
-        <button onClick={handleSidebar}>
-          {!showSidebar ? (
-            <img className="menu" src={Menu} alt="" />
-          ) : (
-            <img className="close" src={Close} />
-          )}
-        </button>
-      </div>
-      <div className="nav-list">
-        <HashLink to="/#our-services1">
-          <p> Our Services</p>
-        </HashLink>
-        <HashLink to="/#why-us">
-          <p>Why Us</p>
-        </HashLink>
-        <HashLink to="/#testimony">
-          <p>Testimonial</p>
-        </HashLink>
-        <HashLink to="/#faq">
-          <p>FAQ</p>
-        </HashLink>
-        {access_Token ? ( //Kondisi terlebih dahulu kemudian link
-          <HashLink
-            onKeyDown={handleLogout}
-            className="register"
-            to="/sign-in"
-            onClick={handleLogout}>
-            <p>Logout</p>
+      <div className="container-button">
+        <Sidebar />
+        <div className="nav-list">
+          <HashLink to="/#our-services1">
+            <p> Our Services</p>
           </HashLink>
-        ) : (
-          <Link className="register" to={"/sign-in"}>
-            register
-          </Link>
-        )}
+          <HashLink to="/#why-us">
+            <p>Why Us</p>
+          </HashLink>
+          <HashLink to="/#testimony">
+            <p>Testimonial</p>
+          </HashLink>
+          <HashLink to="/#faq">
+            <p>FAQ</p>
+          </HashLink>
+          {access_Token ? ( //Kondisi terlebih dahulu kemudian link
+            <HashLink
+              onKeyDown={handleLogout}
+              className="register"
+              to="/sign-in"
+              onClick={handleLogout}>
+              <p>Logout</p>
+            </HashLink>
+          ) : (
+            <Link className="register" to={"/sign-in"}>
+              register
+            </Link>
+          )}
+        </div>
       </div>
     </div>
   );
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,38 +1,49 @@
 import React from "react";
 import "./style.css";
-import { Link } from "react-router-dom";
 import { useState } from "react";
 import Menu from "../../assets/icon/fi_menu.png";
-import Close from "../../assets/icon/fi_x.png";
+import Button from "react-bootstrap/Button";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import { HashLink } from "react-router-hash-link";
 
 const Sidebar = () => {
-  const [showSidebar, setshowSidebar] = useState(false);
-
-  const handleSidebar = () => {
-    setshowSidebar(!showSidebar);
-  };
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
   return (
-    <>
-      <div className="overlay"></div>
-      <div className="sidebar">
-        <h4>BCR</h4>
-        <div className="list-menu">
-          <a onClick={handleSidebar} href="#our-services">
-            <p> Our Services</p>
-          </a>
-          <a href="#why-us">
-            <p>Why Us</p>
-          </a>
-          <a href="#testimony">
-            <p>Testimonial</p>
-          </a>
-          <a href="#our-services">
-            <p>FAQ</p>
-          </a>
-        </div>
-      </div>
-    </>
+    <div className="sidebar">
+      <Button variant="primary" className="button" onClick={handleShow}>
+        <img src={Menu} alt="" />
+      </Button>
+      <Offcanvas
+        show={show}
+        onHide={handleClose}
+        placement="end"
+        restoreFocus={false}>
+        <Offcanvas.Header className="text-reset" closeButton>
+          <Offcanvas.Title>BCR</Offcanvas.Title>
+        </Offcanvas.Header>
+        <Offcanvas.Body>
+          <div className="sidebar-list">
+            <div className="list-menu d-flex flex-column align-items-start gap-3 ">
+              <HashLink className="btn" to={"/#our-services1"}>
+                Our Services{" "}
+              </HashLink>
+              <button className="btn">
+                <a href="#why-us">Why Us</a>
+              </button>
+              <button className="btn">
+                <a href="#testimony">Testimonial </a>
+              </button>
+              <button className="btn">
+                <a href="#faq">FAQ </a>
+              </button>
+            </div>
+          </div>
+        </Offcanvas.Body>
+      </Offcanvas>
+    </div>
   );
 };
 
